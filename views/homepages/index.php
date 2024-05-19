51c30bb1b480bf44e8e0e61c497daec23164700a
Robindebie <335833@student.mboutrecht.nl>
2024-05-19 13:50:17 +0200

    tweede commit

--- views/homepages/index.php
+++ views/homepages/index.php
@@ -9,6 +9,7 @@
 
             <a href="<?= URLROOT; ?>/instructeurs/index">Instructeurs</a> |
             <a href="<?= URLROOT; ?>/Countries/index">Landen van de Wereld</a> |
+            <a href="<?= URLROOT; ?>/nailstudio/index">nailstudio</a> |
 
         </div>
         <div class="col-3"></div>
---
+++ views/nailstudio/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>blingblingnagelstudiochantal</title>
+</head>
+<body>
+    <h1>
+        Bling Bling Nail Studio Chantal
+    </h1>
+
+    <h3>
+        kies 4 basiskleuren voor uw nagels:
+    </h3>
+    <label for=""></label>
+    <input type="color" value="#ff0000">
+    <input type="color" value="#712995">
+    <input type="color" value="#DA98CA">
+    <input type="color" value="#ECE46C">
+
+    <h3>
+        Uw telefoonnummer:
+    </h3>
+    <input type="tel" id="phone" placeholder="+31 6 2570 51 41" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}">
+    <form>
+  
+
+</body>
+</html>
